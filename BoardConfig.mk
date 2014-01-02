f173072308d377c0e3b346d9d5bd2245a7916ffc
beerbong <yghazi@gmail.com>
2014-01-02 17:25:05 +0100

    Add dependencies and build CWM
    Change-Id: I618c9f362d131181eee03d33677f19a7010ba8ac

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,8 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/togari/bluetooth
 # Kernel properties
 TARGET_KERNEL_CONFIG := cm_rhine_togari_row_defconfig
 
+BOARD_RECOVERY_CWM := true
+
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26
 
